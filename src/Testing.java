d419e54d6b2c73926584c7a6ccafeb021a0b3adf
Tobias <Tobias.as@hotmail.com>
2020-09-01 14:32:05 +0200

    Merge branch 'master' of C:\Users\Anker\Desktop\2. sem KEA\GitEks with conflicts.

--- src/Testing.java
+++ src/Testing.java
@@ -2,7 +2,6 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println(";D");
-        System.out.println("Denne er nu pushed?"); //Rettet fejl her
-        System.out.println("Lol bro");
+        System.out.println("denne er nu pushed?");
     }
 }
