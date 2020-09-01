61ce5ef47466d1f1c7ee9404abbabb76b9bb6e49
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2020-09-01 14:21:56 +0200

    new version from Emil

--- src/Testing.java
+++ src/Testing.java
@@ -2,6 +2,7 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println(";D");
-        System.out.println("denne er nu pushed?");
+        System.out.println("Denne er nu pushed?"); //Rettet fejl her
+        System.out.println("Lol bro");
     }
 }
