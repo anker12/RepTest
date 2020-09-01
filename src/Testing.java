98efc1ba530b71208f209971b20bfeba3efbe32c
Tobias <Tobias.as@hotmail.com>
2020-09-01 14:10:23 +0200

    andet

--- src/Testing.java
+++ src/Testing.java
@@ -2,5 +2,6 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println(";D");
+        System.out.println("denne er nu pushed?");
     }
 }
